12ada853f08bd3fc3c1ae943a842b455325e4134
cattmassidy528 <cattmassidy528@gmail.com>
2024-03-07 18:35:09 -0500

    message

--- server/server.js
+++ server/server.js
@@ -32,10 +32,10 @@ app.get("/", (req, res) => {
 });
 
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
