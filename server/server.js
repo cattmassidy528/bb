3e0de03f58487046fea73c0c3d1fd55f28c34eda
cattmassidy528 <cattmassidy528@gmail.com>
2024-03-07 22:05:29 -0500

    message

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const path = require('path')
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const authRoutes = require("./routes/authRoutes");
 const mainRoutes = require("./routes/mainRoutes");
 
@@ -40,6 +40,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
