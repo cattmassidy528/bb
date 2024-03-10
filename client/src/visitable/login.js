cfbc6b0d7470e30c188e298fd9e515dca79969d4
cattmassidy528 <cattmassidy528@gmail.com>
2024-03-09 23:02:51 -0500

    ...

--- client/src/visitable/login.js
+++ client/src/visitable/login.js
@@ -121,7 +121,7 @@ const Login = ({ loginOrRegister, setLoginOrRegister }) => {
   };
 
   return (
-    <div className="d-flex justify-content-center ">
+    <div className="d-flex justify-content-center align-items-center">
       <div className="card mx-auto my-auto col-md-12 col-lg-12 col-sm-12" style={{ border: "none" }}>
         <div className="d-flex justify-content-center fs-5">returning customers sign in below</div>
         <div className="row d-flex my-auto">
