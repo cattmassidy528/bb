7d1fb39cb497aaf037b8fd6cc9ed4d48f55a5f15
cattmassidy528 <cattmassidy528@gmail.com>
2024-03-11 23:50:33 -0400

    ...

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -7,6 +7,6 @@ const userSchema = new mongoose.Schema({
   balance: { type: Number, default: 1000 },
 });
 
-const User = mongoose.model("User", userSchema, "users");
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
