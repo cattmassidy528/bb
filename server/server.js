86ddef542632a8fa5ab977c92f303bd0dd6528d6
cattmassidy528 <cattmassidy528@gmail.com>
2024-03-13 00:07:22 -0400

    ...

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 // const bcryptjs = require("bcryptjs");
 const path = require('path')
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 
 app.use(cors());
 app.use(express.json());
@@ -14,7 +14,7 @@ app.use(express.json());
 const authRoutes = require("./routes/authRoutes");
 const mainRoutes = require("./routes/mainRoutes");
 
-const uri = process.env.MONGODB_URI
+const uri = "mongodb+srv://admin:123passwd321@bb-mongo-1.osmaqiw.mongodb.net/bb-mongo-1?retryWrites=true&w=majority&appName=bb-mongo-1"
 
 mongoose
   .connect(uri, {
@@ -42,6 +42,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-// app.listen(port, () => {
-//   console.log(`Server is running on PORT ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+});
