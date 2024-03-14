383602283dd4a5a4efcae23ef1640da5e54c5724
cattmassidy528 <cattmassidy528@gmail.com>
2024-03-14 17:00:47 -0400

    ...

--- server/server.js
+++ server/server.js
@@ -3,16 +3,19 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-// const bcryptjs = require("bcryptjs");
 const path = require('path')
 const PORT = process.env.PORT || 5000;
+
+const authRoutes = require("./routes/authRoutes");
+const mainRoutes = require("./routes/mainRoutes");
 
 app.use(cors());
 app.use(express.json());
 
 
-const authRoutes = require("./routes/authRoutes");
-const mainRoutes = require("./routes/mainRoutes");
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
 
 const uri = "mongodb+srv://admin:123passwd321@bb-mongo-1.osmaqiw.mongodb.net/bb-mongo-1?retryWrites=true&w=majority&appName=bb-mongo-1"
 
@@ -36,12 +39,16 @@ app.get("/", (req, res) => {
   res.send({ message: "hello from express nightmare." });
 });
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Internal Server Error");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
+
+
+
