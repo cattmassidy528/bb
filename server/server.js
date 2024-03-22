3c4a7b3fb76238f5331fa497a6c185908170708e
cattmassidy528 <cattmassidy528@gmail.com>
2024-03-22 17:27:38 -0400

    ...

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ if (cluster.isMaster) {
 
   // Connect to MongoDB
   mongoose
-    .connect(process.env.MONGODB_URI, {
+    .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bb-mongo-1', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
