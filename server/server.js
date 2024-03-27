e44fe0549f66628911c081795651b63da98fa1ff
cattmassidy528 <cattmassidy528@gmail.com>
2024-03-27 02:18:46 -0400

    ...

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -9,44 +8,42 @@ const PORT = process.env.PORT || 5000;
 const authRoutes = require("./routes/authRoutes");
 const mainRoutes = require("./routes/mainRoutes");
 
-app.use(cors());
-app.use(express.json());
+// Use CORS middleware
+const corsOptions = {
+  origin: 'https://bad-bank-matthew-cassidy-709735df14a5.herokuapp.com',
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
-app.use(cors({ origin: 'https://bad-bank-matthew-cassidy-709735df14a5.herokuapp.com/' }))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-});
+// Serve static files from the React build directory
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-// const uri = "mongodb+srv://admin:123passwd321@bb-mongo-1.osmaqiw.mongodb.net/bb-mongo-1?retryWrites=true&w=majority&appName=bb-mongo-1"
-
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
-
-
+// Define API routes
 app.use("/api/auth", authRoutes);
 app.use("/api/auth/profile", mainRoutes);
 
+// Serve the React app for any other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((error) => {
+  console.error("Error connecting to MongoDB:", error);
+});
 
-// app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Internal Server Error");
 });
 
-
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
-
-
-
